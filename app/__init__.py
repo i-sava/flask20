8a8c9f986226973176fc886e2c9dcd5a84701354
i-sava <voynyliv@ukr.net>
2020-11-10 15:47:14 +0200

    login_required

--- app/__init__.py
+++ app/__init__.py
@@ -8,5 +8,8 @@ app.config.from_object('config')
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+login_manager.login_message_category = 'info'
+
 
 from app import views, models
